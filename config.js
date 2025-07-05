5b726ffbbd0b07457d630f2397ff8ca32ce678f1
Anil <70024200+destinynokha@users.noreply.github.com>
2025-07-05 12:13:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// config.js (DO NOT COMMIT THIS TO GITHUB)
-
 function decodePassword(encoded, key = 7) {
   return encoded.split('').map(char =>
     String.fromCharCode(char.charCodeAt(0) ^ key)
@@ -10,19 +8,19 @@ const BRANCH_CONFIG = {
   nokha: {
     name: "NOKHA TYRES",
     username: "naresh",
-    password: decodePassword("ihran`20"), // "nokha123"
+    password: decodePassword("ihlof654"), // "nokha123"
     apiUrl: "https://script.google.com/macros/s/AKfycbym7YGcofE9savrP00LEpehuGSZPlGwM5tHd7kucL_HPiwZI6F_YXOLx2I_YtvT5IJUUg/exec"
   },
   honda: {
     name: "HONDA",
     username: "radheshyam",
-    password: decodePassword("mhdkfd20"), // "honda123"
+    password: decodePassword("idkda654"), // "honda123"
     apiUrl: "https://script.google.com/macros/s/AKfycbw-YClOVhPyxUp0AQM3m7261embbCmO-BeM-ZGQmSewC2qvLN8i8yvZGU5Aeg7HONcs/exec"
   },
   bikaner: {
     name: "BIKANER MAIN",
     username: "sishpal",
-    password: decodePassword("chheklfd20"), // "bikaner123"
+    password: decodePassword("cildkhld654"), // "bikaner123"
     apiUrl: "https://script.google.com/macros/s/AKfycbwJlH4BxuDKmyJndQzWXW65Ik7DuYdJvyFPzFishfvOZz76-kok6MAupf2wZazlh_CO/exec"
   }
 };
