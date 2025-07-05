f2cdcd875e5f16b0759fc2a31438fe45904250b6
Anil <70024200+destinynokha@users.noreply.github.com>
2025-07-05 12:30:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,19 +8,19 @@ const BRANCH_CONFIG = {
   nokha: {
     name: "NOKHA TYRES",
     username: "naresh",
-    password: decodePassword("ihlof654"), // "nokha123"
+    password: decodePassword("ihlof654"), 
     apiUrl: "https://script.google.com/macros/s/AKfycbym7YGcofE9savrP00LEpehuGSZPlGwM5tHd7kucL_HPiwZI6F_YXOLx2I_YtvT5IJUUg/exec"
   },
   honda: {
     name: "HONDA",
     username: "radheshyam",
-    password: decodePassword("ohicf654"), // "honda123"
+    password: decodePassword("ohicf654"), //
     apiUrl: "https://script.google.com/macros/s/AKfycbw-YClOVhPyxUp0AQM3m7261embbCmO-BeM-ZGQmSewC2qvLN8i8yvZGU5Aeg7HONcs/exec"
   },
   bikaner: {
     name: "BIKANER MAIN",
     username: "sishpal",
-    password: decodePassword("enlfibu654"), // "bikaner123"
+    password: decodePassword("enlfibu654"), // 
     apiUrl: "https://script.google.com/macros/s/AKfycbwJlH4BxuDKmyJndQzWXW65Ik7DuYdJvyFPzFishfvOZz76-kok6MAupf2wZazlh_CO/exec"
   }
 };
